c272b93affe1d7da289505c9cf0564f513926ba0
Dusan <dn185116@ncr.com>
2023-02-06 09:00:42 +0100

    .

--- VaucherPracticeSerice/Program.cs
+++ VaucherPracticeSerice/Program.cs
@@ -2,6 +2,9 @@ using Serilog;
 using VaucherPracticeSerice;
 using VaucherPracticeService.Services.VPS;
 
+
+// msiexec /i VaucherService.msi TARGETDIR=C:\ APSBASEURL=https://test APSUSERNAME=ARA6/RMH14== APSPASSWORD=WjVFMVdTcGViaw== APSREQUESTTIMEOUTSECONDS=30 APSPROMPTCASHIERTIMEOUTSECONDS=20 APSDISPLAYMESSAGETIMEOUTSECONDS=5 PINPADIPADDRESS=127.0.0.1 PINPADPORT=2008 PINPADRECEIVETIMEOUTMILIS=60000 POLLINGPERIODMILIS=2000 CASHBACKPROMPTENABLED=false /quiet
+
 IHost host = Host.CreateDefaultBuilder(args)
      .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
             .ReadFrom.Configuration(hostingContext.Configuration)
